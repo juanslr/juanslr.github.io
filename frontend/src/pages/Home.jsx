49d4f2fda656b88e94d0b57ba397998be92414e6
Juan.s.l.r <45296794+juanslr@users.noreply.github.com>
2025-04-28 09:03:50 +0200

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,6 +17,7 @@ const Home = () => {
       >
         Únete a la Comunidad
       </a>
+          <iframe src="https://discord.com/widget?id=762312334642642984&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
     </section>
   );
 };
