d7e463c95096f360744f09b0db6fc14ba0388928
Juan.s.l.r <45296794+juanslr@users.noreply.github.com>
2025-08-06 13:59:59 +0200

    Update Home.jsx
    Espacios Eliminados

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -19,8 +19,6 @@ const Home = () => {
       </a>
     </section>
     <center><iframe src="https://discord.com/widget?id=762312334642642984&amp;theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe></center>
-
   );
 };
-
 export default Home;
