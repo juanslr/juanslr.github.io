3a96d7494048dba055e52c23a521817a1169725c
juanslr <mimenteesunica@gmail.com>
2025-08-06 15:35:06 +0200

    hashroute-fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Comunidad from './pages/Comunidad';
 import Contacto from './pages/Contacto';
-import UnsupportedBrowserModal from './UnsupportedBrowserModal'; // Importamos el nuevo modal
+import UnsupportedBrowserModal from './UnsupportedBrowserModal';
 
 function App() {
   return (
     <Router>
       <div className="bg-gray-900 min-h-screen text-white">
-        {/* Mostrar advertencia si el navegador es incompatible */}
         <UnsupportedBrowserModal />
-
-        {/* Barra de navegación */}
         <Navbar />
-
-        {/* Rutas */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/comunidad" element={<Comunidad />} />
